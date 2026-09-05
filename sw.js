b1f65ea2f95a13bd70ad88794f5a5d8388272385
danielcross-byte <daniellauridsen2@gmail.com>
2026-09-05 22:21:30 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE='cross-service-v38-focused-images';
-const CORE=['./','./index.html','./manifest.json','./bike_highres_crop.png'];
-const DETAIL=['./service_images/gear.jpg','./service_images/airfilter.jpg','./service_images/chain.jpg','./service_images/frontbrake.jpg','./service_images/tire.jpg','./service_images/spark.jpg','./service_images/carb.jpg','./service_images/piston.jpg','./service_images/exhaust.jpg','./service_images/cooling.jpg','./service_images/suspension.jpg','./service_images/steering.jpg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE.concat(DETAIL))))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const url=new URL(e.request.url);if(url.pathname.endsWith('/index.html')||url.pathname.endsWith('/')){e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return r}).catch(()=>caches.match('./index.html')));return}e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r})))})
