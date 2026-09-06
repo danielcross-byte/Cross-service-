fe7ff0a4475d40e840a56944d543f7176cba4b7b
danielcross-byte <daniellauridsen2@gmail.com>
2026-09-06 22:43:00 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cross-service-v47-clean-overview';
+const CACHE='cross-service-v48-timer-modal';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./bike_highres_crop.png','./gear.jpg','./airfilter.jpg','./chain.jpg','./frontbrake.jpg','./tire.jpg','./spark.jpg','./carb.jpg','./piston.jpg','./exhaust.jpg','./cooling.jpg','./suspension.jpg','./steering.jpg']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
